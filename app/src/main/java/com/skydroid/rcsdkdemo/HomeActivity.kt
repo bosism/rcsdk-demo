029ac03078cf1f974c513adb8e7c8268679fd451
咔一下 <1501020210@qq.com>
2023-10-16 14:44:06 +0800

    完善开发示例

--- app/src/main/java/com/skydroid/rcsdkdemo/HomeActivity.kt
+++ app/src/main/java/com/skydroid/rcsdkdemo/HomeActivity.kt
@@ -1,22 +1,29 @@
 package com.skydroid.rcsdkdemo
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.skydroid.rcsdk.KeyManager
-import com.skydroid.rcsdk.PipelineManager
+import com.skydroid.rcsdk.PipelineManager.connectPipeline
+import com.skydroid.rcsdk.PipelineManager.createPipeline
+import com.skydroid.rcsdk.PipelineManager.disconnectPipeline
 import com.skydroid.rcsdk.RCSDKManager
 import com.skydroid.rcsdk.SDKManagerCallBack
 import com.skydroid.rcsdk.comm.CommListener
 import com.skydroid.rcsdk.common.DeviceType
-import com.skydroid.rcsdk.common.remotecontroller.ControlMode
+import com.skydroid.rcsdk.common.Uart
 import com.skydroid.rcsdk.common.callback.CompletionCallbackWith
 import com.skydroid.rcsdk.common.callback.KeyListener
 import com.skydroid.rcsdk.common.error.SkyException
 import com.skydroid.rcsdk.common.pipeline.Pipeline
+import com.skydroid.rcsdk.common.remotecontroller.ControlMode
 import com.skydroid.rcsdk.key.AirLinkKey
 import com.skydroid.rcsdk.key.RemoteControllerKey
+import java.util.*
 
 /**
  * @author 咔一下
@@ -28,117 +35,196 @@ class HomeActivity: AppCompatActivity() {
 
     val TAG = "HomeActivity"
 
-    private var keySignalQualityListener = KeyListener<Int>{
-        oldValue, newValue ->
-        Log.e(TAG,"信号强度:${oldValue},${newValue}")
-    }
+    private val keySignalQualityListener =
+        KeyListener<Int> { oldValue, newValue ->
+            strSignalValue = "信号强度:$newValue"
+            printInfo(InfoKey.Signal, strSignalValue)
+        }
 
-    private var pipeline:Pipeline? = null
+    private val keyH16ChannelsListener: KeyListener<IntArray> =
+        KeyListener { oldValue, newValue ->
+            strH16ChannelsValue = Arrays.toString(newValue)
+            printInfo(InfoKey.H16Channels, strH16ChannelsValue)
+        }
+
+    private val infoLiveData = MutableLiveData<String>()
+    private var tvInfo: TextView? = null
+    private var strSignalValue = ""
+    private var strH16ChannelsValue = ""
+    private var strOtherValue = ""
+
+    private var pipeline: Pipeline? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        tvInfo = findViewById(R.id.tv_info)
+        infoLiveData.observe(this) { str -> tvInfo?.text = str }
         //初始化SDK
-        RCSDKManager.initSDK(this,object : SDKManagerCallBack{
+        RCSDKManager.initSDK(this, object : SDKManagerCallBack {
             override fun onRcConnected() {
-                KeyManager.set(RemoteControllerKey.KeyControlMode, ControlMode.JP) {
-                        e ->
-                    if (e == null){
-                        log("设置摇杆模式成功")
-                    }else{
-                        log("设置摇杆模式失败：${e}") }
+                //创建通讯管道(内部有断开重连机制，只需要调用一次连接即可)
+                val pipeline = createPipeline(Uart.UART0)
+                pipeline!!.onCommListener = object : CommListener {
+                    override fun onConnectSuccess() {
+                        log("管道连接成功")
                     }
 
-
-                KeyManager.get(RemoteControllerKey.KeyControlMode,object :
-                    CompletionCallbackWith<ControlMode> {
-                    override fun onSuccess(result: ControlMode?) {
-                        log(result)
+                    override fun onConnectFail(e: SkyException) {
+                        log("管道连接失败$e")
                     }
 
-                    override fun onFailure(e: SkyException?) {
-                        log(e)
+                    override fun onDisconnect() {
+                        log("管道断开连接")
+                    }
+
+                    override fun onReadData(bytes: ByteArray) {}
+                }
+                //连接通讯管道
+                connectPipeline(pipeline)
+                this@HomeActivity.pipeline = pipeline
+            }
+
+            override fun onRcConnectFail(e: SkyException?) {}
+            override fun onRcDisconnect() {}
+        })
+        //连接到遥控器
+        RCSDKManager.connectToRC()
+        initTestView()
+    }
+
+    private fun initTestView() {
+        findViewById<View>(R.id.btn_pairing).setOnClickListener {
+            KeyManager.action(RemoteControllerKey.KeyRequestPairing) { e ->
+                if (e == null) {
+                    printInfo(InfoKey.Other, "对频成功")
+                } else {
+                    printInfo(InfoKey.Other, "对频失败：$e")
+                }
+            }
+        }
+        findViewById<View>(R.id.btn_set_control_mode).setOnClickListener {
+            KeyManager.set(RemoteControllerKey.KeyControlMode, ControlMode.USA) { e ->
+                if (e == null) {
+                    printInfo(InfoKey.Other, "设置摇杆模式成功")
+                } else {
+                    printInfo(InfoKey.Other, "设置摇杆模式失败：$e")
+                }
+            }
+        }
+        findViewById<View>(R.id.btn_get_control_mode).setOnClickListener { //获取遥控器手型模式
+            KeyManager.get(RemoteControllerKey.KeyControlMode, object : CompletionCallbackWith<ControlMode> {
+                    override fun onSuccess(controlMode: ControlMode) {
+                        printInfo(InfoKey.Other, "获取摇杆模式：" + controlMode.name)
+                    }
+
+                    override fun onFailure(e: SkyException) {
+                        printInfo(InfoKey.Other, "获取摇杆模式失败：$e")
                     }
                 })
+        }
+        findViewById<View>(R.id.btn_get_channels).setOnClickListener {
+            //获取摇杆杆量
+            when (RCSDKManager.getDeviceType()) {
+                DeviceType.H16 -> {
+                    //防止反复监听
+                    KeyManager.cancelListen(keyH16ChannelsListener)
+                    //H16/H16Pro的摇杆杆量为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                    KeyManager.listen(RemoteControllerKey.KeyH16Channels, keyH16ChannelsListener)
+                }
+                //H12/H12Pro/H30摇杆杆量为GET方式，需要主动请求，请求一次获取一次
+                else ->{
+                    KeyManager.get(RemoteControllerKey.KeyChannels,object : CompletionCallbackWith<IntArray> {
+                        override fun onSuccess(value: IntArray?) {
+                            printInfo(InfoKey.Other, "获取摇杆杆量：" + Arrays.toString(value))
+                        }
 
-                when(RCSDKManager.getDeviceType()){
-                    DeviceType.H12 -> {//H12的信号强度为GET方式，需要主动请求，请求一次获取一次
-                        KeyManager.get(AirLinkKey.KeyH12SignalQuality,object : CompletionCallbackWith<Int>{
-                            override fun onSuccess(result: Int?) {
-                                log("H12信号强度：${result}")
+                        override fun onFailure(e: SkyException) {
+                            printInfo(InfoKey.Other, "获取摇杆失败：$e")
+                        }
+                    })
+                }
+
+            }
+        }
+        findViewById<View>(R.id.btn_get_signal).setOnClickListener {
+            when (RCSDKManager.getDeviceType()) {
+                DeviceType.H12 ->                         //H12的信号强度为GET方式，需要主动请求，请求一次获取一次
+                    KeyManager.get(AirLinkKey.KeyH12SignalQuality, object : CompletionCallbackWith<Int> {
+                            override fun onSuccess(integer: Int) {
+                                printInfo(InfoKey.Other, "H12信号强度：$integer")
                             }
 
-                            override fun onFailure(e: SkyException?) {
-
+                            override fun onFailure(e: SkyException) {
+                                printInfo(InfoKey.Other, "H12信号强度获取失败：$e")
                             }
                         })
-                    }
-                    DeviceType.H12Pro -> {//H12Pro的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
-                        KeyManager.listen(AirLinkKey.KeySignalQuality,keySignalQualityListener)
-                    }
-                    DeviceType.H16 -> {//H16的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
-                        KeyManager.listen(AirLinkKey.KeyH16SignalQuality,keySignalQualityListener)
-                    }
+                DeviceType.H12Pro -> {
+                    //防止反复监听
+                    KeyManager.cancelListen(keySignalQualityListener)
+                    //H12Pro的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                    KeyManager.listen(
+                        AirLinkKey.KeySignalQuality,
+                        keySignalQualityListener
+                    )
                 }
-                //创建通讯管道(内部有断开重连机制，只需要调用一次连接即可)
-                pipeline = PipelineManager.createPipeline()
-                pipeline?.let {
-                    //设置监听
-                    it.onCommListener = object : CommListener{
-                        override fun onConnectSuccess() {
-                            log("管道连接成功")
-                        }
-
-                        override fun onConnectFail(e: SkyException?) {
-                            log("管道连接失败${e}")
-                        }
-
-                        override fun onDisconnect() {
-                            log("管道断开连接")
-                        }
-
-                        override fun onReadData(data: ByteArray?) {
-
-                        }
-
-                    }
-                    //连接通讯管道
-                    PipelineManager.connectPipeline(it)
-                }
-            }
-
-            override fun onRcConnectFail(e: SkyException?) {
-
-            }
-
-            override fun onRcDisconnect() {
-
-            }
-        })
-        RCSDKManager.connectToRC()
-        findViewById<View>(R.id.btn_pairing).setOnClickListener{
-            KeyManager.action(RemoteControllerKey.KeyRequestPairing){
-                e ->
-                if (e == null){
-                    log("对频成功")
-                }else{
-                    log("对频失败：${e}")
+                DeviceType.H16 -> {
+                    //防止反复监听
+                    KeyManager.cancelListen(keySignalQualityListener)
+                    //H16的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                    KeyManager.listen(
+                        AirLinkKey.KeyH16SignalQuality,
+                        keySignalQualityListener
+                    )
                 }
             }
         }
     }
 
-    private fun log(any: Any?){
-        any ?: return
-        Log.e(TAG,any.toString())
+
+    private fun printInfo(key: InfoKey, obj: Any?) {
+        obj ?: return
+        when (key) {
+            InfoKey.Signal -> strSignalValue = obj.toString()
+            InfoKey.H16Channels -> strH16ChannelsValue = obj.toString()
+            InfoKey.Other -> strOtherValue = obj.toString()
+        }
+        val sb = StringBuffer()
+        if (!TextUtils.isEmpty(strSignalValue)) {
+            sb.append(strSignalValue)
+            sb.append("\n")
+        }
+        if (!TextUtils.isEmpty(strH16ChannelsValue)) {
+            sb.append(strH16ChannelsValue)
+            sb.append("\n")
+        }
+        if (!TextUtils.isEmpty(strOtherValue)) {
+            sb.append(strOtherValue)
+        }
+        infoLiveData.postValue(sb.toString())
+        Log.e(TAG, obj.toString())
+    }
+
+    private fun log(obj: Any?) {
+        if (obj == null) {
+            return
+        }
+        Log.e(TAG, obj.toString())
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        infoLiveData.removeObservers(this)
+        //断开遥控器连接（如果不断开，程序还在运行的时候，其他程序会出端口占用情况）
+        RCSDKManager.disconnectRC()
         KeyManager.cancelListen(keySignalQualityListener)
-
-        pipeline?.let {
-            PipelineManager.disconnectPipeline(it)
+        val p = pipeline
+        if (p != null) {
+            disconnectPipeline(p)
         }
+    }
+
+    internal enum class InfoKey {
+        Signal, H16Channels, Other
     }
 }
--- app/src/main/java/com/skydroid/rcsdkdemo/HomeActivityForJava.java
+++ app/src/main/java/com/skydroid/rcsdkdemo/HomeActivityForJava.java
@@ -1,11 +1,15 @@
 package com.skydroid.rcsdkdemo;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.skydroid.rcsdk.KeyManager;
 import com.skydroid.rcsdk.PipelineManager;
@@ -22,6 +26,8 @@ import com.skydroid.rcsdk.common.remotecontroller.ControlMode;
 import com.skydroid.rcsdk.key.AirLinkKey;
 import com.skydroid.rcsdk.key.RemoteControllerKey;
 
+import java.util.Arrays;
+
 /**
  * @author 咔一下
  * @date 2023/10/11 9:50
@@ -34,9 +40,24 @@ public class HomeActivityForJava extends AppCompatActivity {
     private final KeyListener<Integer> keySignalQualityListener = new KeyListener<Integer>() {
         @Override
         public void onValueChange(Integer oldValue, Integer newValue) {
-            Log.e(TAG,"信号强度:" + oldValue + "," + newValue);
+            strSignalValue = "信号强度:" + newValue;
+            printInfo(InfoKey.Signal,strSignalValue);
         }
     };
+
+    private final KeyListener<int[]> keyH16ChannelsListener = new KeyListener<int[]>() {
+        @Override
+        public void onValueChange(int[] oldValue, int[] newValue) {
+            strH16ChannelsValue = Arrays.toString(newValue);
+            printInfo(InfoKey.H16Channels,strH16ChannelsValue);
+        }
+    };
+
+    private final MutableLiveData<String> infoLiveData = new MutableLiveData<String>();
+    private TextView tvInfo = null;
+    private String strSignalValue = "";
+    private String strH16ChannelsValue = "";
+    private String strOtherValue = "";
 
     private Pipeline pipeline = null;
 
@@ -44,61 +65,17 @@ public class HomeActivityForJava extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tvInfo = findViewById(R.id.tv_info);
+        infoLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String str) {
+                tvInfo.setText(str);
+            }
+        });
         //初始化SDK
         RCSDKManager.INSTANCE.initSDK(this, new SDKManagerCallBack() {
             @Override
             public void onRcConnected() {
-                //设置遥控器手型模式
-                KeyManager.INSTANCE.set(RemoteControllerKey.INSTANCE.getKeyControlMode(), ControlMode.JP, new CompletionCallback() {
-                    @Override
-                    public void onResult(SkyException e) {
-                        if (e == null){
-                            log("设置摇杆模式成功");
-                        }else{
-                            log("设置摇杆模式失败：" + e);
-                        }
-                    }
-                });
-
-                //获取遥控器手型模式
-                KeyManager.INSTANCE.get(RemoteControllerKey.INSTANCE.getKeyControlMode(), new CompletionCallbackWith<ControlMode>() {
-                    @Override
-                    public void onSuccess(ControlMode controlMode) {
-                        log(controlMode);
-                    }
-
-                    @Override
-                    public void onFailure(SkyException e) {
-
-                    }
-                });
-
-                switch (RCSDKManager.INSTANCE.getDeviceType()){
-                    case H12:
-                        //H12的信号强度为GET方式，需要主动请求，请求一次获取一次
-                        KeyManager.INSTANCE.get(AirLinkKey.INSTANCE.getKeyH12SignalQuality(), new CompletionCallbackWith<Integer>() {
-                            @Override
-                            public void onSuccess(Integer integer) {
-                                log("H12信号强度：" + integer);
-                            }
-
-                            @Override
-                            public void onFailure(SkyException e) {
-
-                            }
-                        });
-                        break;
-
-                    case H12Pro:
-                        //H12Pro的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
-                        KeyManager.INSTANCE.listen(AirLinkKey.INSTANCE.getKeySignalQuality(),keySignalQualityListener);
-                        break;
-                    case H16:
-                        //H16的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
-                        KeyManager.INSTANCE.listen(AirLinkKey.INSTANCE.getKeyH16SignalQuality(),keySignalQualityListener);
-                        break;
-                }
-
                 //创建通讯管道(内部有断开重连机制，只需要调用一次连接即可)
                 Pipeline pipeline = PipelineManager.INSTANCE.createPipeline(Uart.UART0);
                 pipeline.setOnCommListener(new CommListener() {
@@ -139,6 +116,10 @@ public class HomeActivityForJava extends AppCompatActivity {
         });
         //连接到遥控器
         RCSDKManager.INSTANCE.connectToRC();
+        initTestView();
+    }
+
+    private void initTestView() {
         findViewById(R.id.btn_pairing).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -146,14 +127,142 @@ public class HomeActivityForJava extends AppCompatActivity {
                     @Override
                     public void onResult(SkyException e) {
                         if (e == null){
-                            log("对频成功");
+                            printInfo(InfoKey.Other,"对频成功");
                         }else{
-                            log("对频失败：" + e);
+                            printInfo(InfoKey.Other,"对频失败：" + e);
                         }
                     }
                 });
             }
         });
+        findViewById(R.id.btn_set_control_mode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeyManager.INSTANCE.set(RemoteControllerKey.INSTANCE.getKeyControlMode(), ControlMode.USA, new CompletionCallback() {
+                    @Override
+                    public void onResult(SkyException e) {
+                        if (e == null){
+                            printInfo(InfoKey.Other,"设置摇杆模式成功");
+                        }else{
+                            printInfo(InfoKey.Other,"设置摇杆模式失败：" + e);
+                        }
+                    }
+                });
+            }
+        });
+        findViewById(R.id.btn_get_control_mode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取遥控器手型模式
+                KeyManager.INSTANCE.get(RemoteControllerKey.INSTANCE.getKeyControlMode(), new CompletionCallbackWith<ControlMode>() {
+                    @Override
+                    public void onSuccess(ControlMode controlMode) {
+                        printInfo(InfoKey.Other,"获取摇杆模式：" + controlMode.name());
+                    }
+
+                    @Override
+                    public void onFailure(SkyException e) {
+                        printInfo(InfoKey.Other,"获取摇杆模式失败：" + e);
+                    }
+                });
+            }
+        });
+        findViewById(R.id.btn_get_channels).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取摇杆杆量
+                switch (RCSDKManager.INSTANCE.getDeviceType()){
+                    case H16:
+                        //防止反复监听
+                        KeyManager.INSTANCE.cancelListen(keyH16ChannelsListener);
+                        //H16/H16Pro的摇杆杆量为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                        KeyManager.INSTANCE.listen(RemoteControllerKey.INSTANCE.getKeyH16Channels(), keyH16ChannelsListener);
+                        break;
+                    default:
+                        //H12/H12Pro/H30摇杆杆量为GET方式，需要主动请求，请求一次获取一次
+                        KeyManager.INSTANCE.get(RemoteControllerKey.INSTANCE.getKeyChannels(), new CompletionCallbackWith<int[]>() {
+                            @Override
+                            public void onSuccess(int[] value) {
+                                printInfo(InfoKey.Other,"获取摇杆杆量：" + Arrays.toString(value));
+                            }
+
+                            @Override
+                            public void onFailure(SkyException e) {
+                                printInfo(InfoKey.Other,"获取摇杆失败：" + e);
+                            }
+                        });
+                        break;
+                }
+            }
+        });
+
+        findViewById(R.id.btn_get_signal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (RCSDKManager.INSTANCE.getDeviceType()){
+                    case H12:
+                        //H12的信号强度为GET方式，需要主动请求，请求一次获取一次
+                        KeyManager.INSTANCE.get(AirLinkKey.INSTANCE.getKeyH12SignalQuality(), new CompletionCallbackWith<Integer>() {
+                            @Override
+                            public void onSuccess(Integer integer) {
+                                printInfo(InfoKey.Other,"H12信号强度：" + integer);
+                            }
+
+                            @Override
+                            public void onFailure(SkyException e) {
+                                printInfo(InfoKey.Other,"H12信号强度获取失败：" + e);
+                            }
+                        });
+                        break;
+
+                    case H12Pro:
+                        //防止反复监听
+                        KeyManager.INSTANCE.cancelListen(keySignalQualityListener);
+                        //H12Pro的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                        KeyManager.INSTANCE.listen(AirLinkKey.INSTANCE.getKeySignalQuality(),keySignalQualityListener);
+                        break;
+                    case H16:
+                        //防止反复监听
+                        KeyManager.INSTANCE.cancelListen(keySignalQualityListener);
+                        //H16的信号强度为LISTEN方式,设置监听器后，会一直回调，直到取消监听
+                        KeyManager.INSTANCE.listen(AirLinkKey.INSTANCE.getKeyH16SignalQuality(),keySignalQualityListener);
+                        break;
+                }
+
+            }
+        });
+    }
+
+
+    private void printInfo(InfoKey key,Object obj){
+        if (obj == null){
+            return;
+        }
+        switch (key){
+            case Signal:
+                strSignalValue = obj.toString();
+                break;
+            case H16Channels:
+                strH16ChannelsValue = obj.toString();
+                break;
+            case Other:
+                strOtherValue = obj.toString();
+                break;
+        }
+        StringBuffer sb = new StringBuffer();
+        if (!TextUtils.isEmpty(strSignalValue)){
+            sb.append(strSignalValue);
+            sb.append("\n");
+        }
+        if (!TextUtils.isEmpty(strH16ChannelsValue)){
+            sb.append(strH16ChannelsValue);
+            sb.append("\n");
+        }
+        if (!TextUtils.isEmpty(strOtherValue)){
+            sb.append(strOtherValue);
+        }
+        infoLiveData.postValue(sb.toString());
+        Log.e(TAG,obj.toString());
     }
 
     private void log(Object obj){
@@ -161,16 +270,24 @@ public class HomeActivityForJava extends AppCompatActivity {
             return;
         }
         Log.e(TAG,obj.toString());
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        infoLiveData.removeObservers(this);
+        //断开遥控器连接（如果不断开，程序还在运行的时候，其他程序会出端口占用情况）
+        RCSDKManager.INSTANCE.disconnectRC();
         KeyManager.INSTANCE.cancelListen(keySignalQualityListener);
         Pipeline p = this.pipeline;
         if (p != null){
             PipelineManager.INSTANCE.disconnectPipeline(p);
         }
+    }
+
+    enum InfoKey{
+        Signal,
+        H16Channels,
+        Other
     }
 }
